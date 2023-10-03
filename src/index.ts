dc5daa93bc5ec04d90071ca59db0d528d8206474
Dante05S <Dante05S@hotmail.com>
2023-10-03 00:19:50 -0500

    :tada: first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+// import './database/config'
+import express from 'express'
+import compression from 'compression'
+import helmet from 'helmet'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import apiRouter from './routes/index'
+
+const app = express()
+const port = process.env.PORT ?? 3001
+
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(compression())
+
+// Routes
+app.use('/api/v1', apiRouter)
+
+// Say hello
+app.get('/', (_req, res) =>
+  res.status(200).json({ message: 'Bienvenido al core de 100 citas!' })
+)
+
+// Local environment
+if (process.env.ENV === 'local') {
+  app.listen(port, () => {
+    console.log(`Servidor escuchando en :${port}`)
+  })
+}
